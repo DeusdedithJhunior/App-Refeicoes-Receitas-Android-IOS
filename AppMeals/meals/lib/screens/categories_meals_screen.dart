e913e4eff095c4dbd05f776a89fd6252d0428b45
Deusdedith Junior <104400883+DeusdedithJhunior@users.noreply.github.com>
2022-08-15 20:28:39 -0300

    showing the meals

--- AppMeals/meals/lib/screens/categories_meals_screen.dart
+++ AppMeals/meals/lib/screens/categories_meals_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../models/category.dart';
+import '../data/dummy_data.dart';
 
 //aqui será a classe que terá todas as comidas referentes as suas
 //respectivas categorias
@@ -9,12 +10,20 @@ class CategoriesMealsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final category = ModalRoute.of(context)?.settings.arguments as Category;
+
+    final categoryMeals = dummyMeals.where((meal) {
+      return meal.categories.contains(category.id);
+    }).toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(category.title),
       ),
-      body: const Center(
-        child: Text('Receitas por Categoria'),
+      body: ListView.builder(
+        itemCount: categoryMeals.length,
+        itemBuilder: (ctx, index) {
+          return Text(categoryMeals[index].title);
+        },
       ),
     );
   }
