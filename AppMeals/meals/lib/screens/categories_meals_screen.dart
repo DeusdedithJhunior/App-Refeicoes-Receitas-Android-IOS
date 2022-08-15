2cf7b434a645c66a2e2bd3a600c69276a0d935d4
Deusdedith Junior <104400883+DeusdedithJhunior@users.noreply.github.com>
2022-08-15 19:42:59 -0300

    creating named route

--- AppMeals/meals/lib/screens/categories_meals_screen.dart
+++ AppMeals/meals/lib/screens/categories_meals_screen.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
+import '../models/category.dart';
 
 //aqui será a classe que terá todas as comidas referentes as suas
 //respectivas categorias
 class CategoriesMealsScreen extends StatelessWidget {
-  const CategoriesMealsScreen({
-    Key? key,
-  }) : super(key: key);
+  final Category category;
+
+  // ignore: use_key_in_widget_constructors
+  const CategoriesMealsScreen(this.category);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Receitas'),
+        title: Text(category.title),
       ),
       body: const Center(
         child: Text('Receitas por Categoria'),
