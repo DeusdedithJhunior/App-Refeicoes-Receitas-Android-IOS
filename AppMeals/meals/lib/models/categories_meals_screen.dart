a1ed8bae707e723e2b1567cd5b414b9c505b8c04
Deusdedith Junior <104400883+DeusdedithJhunior@users.noreply.github.com>
2022-08-15 18:59:09 -0300

    add page meals

---
+++ AppMeals/meals/lib/models/categories_meals_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+//aqui será a classe que terá todas as comidas referentes as suas
+//respectivas categorias
+class CategoriesMealsScreen extends StatelessWidget {
+  const CategoriesMealsScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
