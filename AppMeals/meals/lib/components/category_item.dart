98a98d477c03e116a28fe30c47adfc06f0e46601
Deusdedith Junior <104400883+DeusdedithJhunior@users.noreply.github.com>
2022-08-15 19:20:50 -0300

    navigation between category and meals screen

--- AppMeals/meals/lib/components/category_item.dart
+++ AppMeals/meals/lib/components/category_item.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import '../models/category.dart';
-import '../screens/categories_screen.dart';
+import '../screens/categories_meals_screen.dart';
 
 //aqui ficará a classe que irá "converter" a base de dados
 //em um componente visual, que será mostrado dentro de categories screen
@@ -13,12 +13,23 @@ class CategoryItem extends StatelessWidget {
     required this.category,
   }) : super(key: key);
 
+  // metodo (ação) que irá chamar a tela categories meals screen
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (_) {
+          return const CategoriesMealsScreen();
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return InkWell(
       splashColor: Theme.of(context).primaryColor,
       borderRadius: BorderRadius.circular(15),
-      onTap: () {},
+      onTap: () => _selectCategory(context),
       child: Container(
         padding: const EdgeInsets.all(15),
         // ignore: sort_child_properties_last
