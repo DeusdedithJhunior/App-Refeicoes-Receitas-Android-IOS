ce21fc172540acf83b84286a747c074607e78294
Deusdedith Junior <104400883+DeusdedithJhunior@users.noreply.github.com>
2022-08-15 16:21:16 -0300

    add fonts

--- AppMeals/meals/lib/components/category_item.dart
+++ AppMeals/meals/lib/components/category_item.dart
@@ -7,10 +7,22 @@ class CategoryItem extends StatelessWidget {
   //variavel que irá receber os atributos de classe category via parametro
   final Category category;
 
-  const CategoryItem({Key? key, required this.category}) : super(key: key);
+  const CategoryItem({
+    Key? key,
+    required this.category,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Text(category.title);
+    return Container(
+      padding: const EdgeInsets.all(15),
+      child: Text(category.title),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(
+              colors: [category.color.withOpacity(0.5), category.color],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight)),
+    );
   }
 }
