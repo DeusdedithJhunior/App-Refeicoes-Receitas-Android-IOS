6d5af58f805345082cd67a9347c2d976b5b288b6
Deusdedith Junior <104400883+DeusdedithJhunior@users.noreply.github.com>
2022-08-15 23:26:43 -0300

    calling the meal in MealDetailScreen

--- AppMeals/meals/lib/screens/meal_detail_screen.dart
+++ AppMeals/meals/lib/screens/meal_detail_screen.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:meals/models/meal.dart';
 
 class MealDetailScreen extends StatelessWidget {
   const MealDetailScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final mealDetail = ModalRoute.of(context)?.settings.arguments as Meal;
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Detalhes da Refeição'),
+        title: Text(mealDetail.title),
       ),
       body: const Center(
         child: Text('Detalhes da Refeição'),
