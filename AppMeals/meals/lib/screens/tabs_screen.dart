f55ad7e3d77adb514b53f16068cfff0abb0f4115
Deusdedith Junior <104400883+DeusdedithJhunior@users.noreply.github.com>
2022-08-16 00:53:14 -0300

    appbar auto titles

--- AppMeals/meals/lib/screens/tabs_screen.dart
+++ AppMeals/meals/lib/screens/tabs_screen.dart
@@ -13,9 +13,9 @@ class TabsScreen extends StatefulWidget {
 class _TabsScreenState extends State<TabsScreen> {
   int _selectedScreenIndex = 0;
 
-  final List<Widget> _screens = [
-    const CategoriesScreen(),
-    const FavoriteScreen(),
+  final List<Map<String, Object>> _screens = [
+    {'title': 'Lista de Categorias', 'screen': const CategoriesScreen()},
+    {'title': 'Meus Favoritos', 'screen': const FavoriteScreen()}
   ];
 
   _selectScreen(int index) {
@@ -28,9 +28,10 @@ class _TabsScreenState extends State<TabsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Vamos Cozinhar?'),
+        title: Center(
+            child: Text(_screens[_selectedScreenIndex]['title'] as String)),
       ),
-      body: _screens[_selectedScreenIndex],
+      body: _screens[_selectedScreenIndex]['screen'] as Widget,
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectScreen,
         backgroundColor: Theme.of(context).colorScheme.primary,
