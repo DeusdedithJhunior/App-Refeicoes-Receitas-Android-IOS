1aff4d17fe79fa4eb4a051645b44a9707b3a9d83
Deusdedith Junior <104400883+DeusdedithJhunior@users.noreply.github.com>
2022-08-15 23:14:57 -0300

    referencing the MealDetailScreen route

--- AppMeals/meals/lib/main.dart
+++ AppMeals/meals/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals/screens/categories_meals_screen.dart';
+import 'package:meals/screens/meal_detail_screen.dart';
 import 'screens/categories_screen.dart';
 import 'utils/app_routes.dart';
 
@@ -29,6 +30,7 @@ class MyApp extends StatelessWidget {
       routes: {
         AppRoutes.home: (ctx) => const CategoriesScreen(),
         AppRoutes.categoriesMeals: (ctx) => const CategoriesMealsScreen(),
+        AppRoutes.mealDetail: (ctx) => const MealDetailScreen()
       },
     );
   }
