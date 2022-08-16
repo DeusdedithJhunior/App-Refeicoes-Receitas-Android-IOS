ec049baf4e2163702f1ee8ab2a6a410fbb1d5654
Deusdedith Junior <104400883+DeusdedithJhunior@users.noreply.github.com>
2022-08-16 00:42:36 -0300

    modifying the main component

---
+++ AppMeals/meals/lib/screens/tabs_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'categories_screen.dart';
+import 'favorite_screen.dart';
+
+// classe que irá controlar a transição entre as tabs
+class TabsScreen extends StatefulWidget {
+  const TabsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<TabsScreen> createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  int _selectedScreenIndex = 0;
+
+  final List<Widget> _screens = [
+    const CategoriesScreen(),
+    const FavoriteScreen(),
+  ];
+
+  _selectScreen(int index) {
+    setState(() {
+      _selectedScreenIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Vamos Cozinhar?'),
+      ),
+      body: _screens[_selectedScreenIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectScreen,
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.category), label: 'Categorias'),
+          BottomNavigationBarItem(icon: Icon(Icons.star), label: 'Favoritos'),
+        ],
+      ),
+    );
+  }
+}
